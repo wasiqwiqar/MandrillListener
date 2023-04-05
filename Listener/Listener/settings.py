29f64b3cb5a3e4dcd4094b2899a7b5cf661d4903
wasiqwiqar <40718572+wasiqwiqar@users.noreply.github.com>
2023-04-05 06:33:14 +0500

    set up daphne & channels

--- Listener/Listener/settings.py
+++ Listener/Listener/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ASGI_APPLICATION = "Listener.asgi.application"
